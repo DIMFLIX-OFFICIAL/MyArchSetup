e7d9ecbe6b900b5230963db7a2b3a7baa071aeef
DIMFLIX-OFFICIAL <dimflix.official@gmail.com>
2024-06-27 17:18:06 +0300

    Изменение местоположения конфигурации polybar

--- config/bspwm/polybar/scripts/launch.sh
+++ config/bspwm/polybar/scripts/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 
 #polybar example  >>/tmp/polybar1.log 2>&1 &
-polybar top -r >>/tmp/polybar1.log 2>&1 & disown
+polybar -c ~/.config/bspwm/polybar/config.ini top -r >>/tmp/polybar1.log 2>&1 & disown
 
 echo "Bars launched..."
 
